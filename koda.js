6f3a460ad1083db433b691d2f844394de5aeef4c
GovcJ <jg7094@student.uni-lj.si>
2016-11-27 23:25:03 +0100

    half map

--- koda.js
+++ koda.js
@@ -19,3 +19,21 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+
+function getLocation() {
+	x = document.getElementById("map");
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+function showPosition(position) {
+    var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center=
+    "+latlon+"&zoom=14&size=400x300&sensor=false/Storžiška+ulica+6,+4000+Kranj";
+
+    document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+}
